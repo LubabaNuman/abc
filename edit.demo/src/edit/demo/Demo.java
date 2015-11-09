a49a62272d1ef4438e7f943801c7e842059c2318
FA12-BSE-233 <FA12-BSE-233@LAB7C-7.cns.ciit>
2015-11-09 10:43:57 +0500

    c3

--- edit.demo/src/edit/demo/Demo.java
+++ edit.demo/src/edit/demo/Demo.java
@@ -5,7 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("Lets do something using GIT");
 		System.out.println("1");
-
+		System.out.println("abc");
 	}
 
 }
